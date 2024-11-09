e7113301d5a3f9784d6f70d424f1bd9ce2425547
rinerte <rinerte@gmail.com>
2024-11-09 17:00:15 +0900

    scaffolding

---
+++ Data/Models/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    internal class Deck
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Card> Cards { get; set; }
+        public DeckType Type { get; set; }
+    }
+}
